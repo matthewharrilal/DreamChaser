dfd02120a5420a0efc3d31af02834ec9ac91d9a7
Matthew Harrilal <matthewharrilal@gmail.com>
2018-10-13 10:15:22 -0700

    Route for new dream from and dream handlebars file

--- app.js
+++ app.js
@@ -7,15 +7,24 @@ import Dream from '/models/dream.js'
 mongoose.Promise = mongoose.connect("mongodb://localhost/dreamchaser");
 
 // Main.handlebars all other templates inherit from
-app.engine('handlebars', exphbs({defaultLayout: 'main'})); // Main Template => main.handlebars
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+})); // Main Template => main.handlebars
 app.set('view engine', 'handlebars');
 
 app.get('/dreams', (req, res) => {
     Dream.find().then(reviews => {
-        res.render('dreams-index', {dreams})
+        res.render('dreams-index', {
+            dreams
+        })
     }).catch(err => {
         console.log(err)
     })
+});
+
+// Submission for new dream form
+app.get('/reviews/new', (req,res) => {
+    res.render('reviews-new', {})
 });
 
 app.listen(3000, () => {
