ac4915ec04c149c5332ddef7ba051768d9d04916
Matthew Harrilal <matthewharrilal@gmail.com>
2018-10-14 15:50:45 -0700

    Moved dream application logic to the controllers folder dream file

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express() // Instantiating Express
 app.use(methodOverride('_method'))
 var exphbs = require('express-handlebars');
 var mongoose = require('mongoose');
-var Dream = require('./models/dream');
+var Dream = require('./controllers/dream.js');
 var ObjectId = require('mongodb').ObjectId
 const bodyParser = require('body-parser');
 
@@ -22,72 +22,8 @@ app.engine('handlebars', exphbs({
 })); // Main Template => main.handlebars
 app.set('view engine', 'handlebars');
 //
-app.get('/dreams', (req, res) => {
-    Dream.find().then(dreams => {
-        // console.log('These are all the dreams ' + dreams)
-        res.render('dreams-index', {
-            dreams
-        })
-    }).catch(err => {
-        console.log(err)
-    })
-});
 
-// Submission for new dream form
-app.get('/dreams/new', (req, res) => {
-    res.render('dream-new', {})
-});
-
-app.post('/dreams', (req, res) => {
-    Dream.create(req.body).then((dream) => {
-        console.log(Object.entries(req.body))
-        res.redirect('/dreams')
-    }).catch((err) => {
-        console.log(err.message)
-    })
-});
-
-app.get('/dreams/:id', (req, res) => {
-    console.log('This is the id ' + req.params.id)
-    Dream.findById({
-        _id: ObjectId(req.params.id)
-    }).then((dream) => {
-        console.log('This is the fetched dream ' + dream)
-        res.render('dream-show', {
-            dream: dream
-        })
-    }).catch((err) => {
-        console.log(err.message)
-    })
-    // res.send('Hello world')
-});
-
-app.put('/dreams/:id', (req, res) => {
-  Dream.findOneAndUpdate(req.params.id, req.body)
-    .then(dream => {
-      console.log('DREAM =>>>> ' + dream)
-      res.redirect(`/dreams/${dream._id}`)
-    })
-    .catch(err => {
-      console.log(err.message)
-    })
-})
-
-
-app.get('/dreams/:id/edit', (req, res) => {
-    console.log('This is the id ' + req.params.id)
-    Dream.findById({
-        _id: ObjectId(req.params.id)
-    }).then((dream) => {
-        console.log('This is the edited dream ' + dream)
-        res.render('dreams-edit', {
-            dream: dream
-        })
-    }).catch((err) => {
-        console.log(err.message)
-    })
-    // res.send('Hello world')
-});
+Dream(app)
 
 
 app.listen(3000, () => {
---
+++ controllers/dream.js
@@ -0,0 +1,73 @@
+const Dream = require('../models/dream.js')
+var ObjectId = require('mongodb').ObjectId
+
+
+module.exports = function(app) {
+    app.get('/dreams', (req, res) => {
+        Dream.find().then(dreams => {
+            // console.log('These are all the dreams ' + dreams)
+            res.render('dreams-index', {
+                dreams
+            })
+        }).catch(err => {
+            console.log(err)
+        })
+    });
+
+    // Submission for new dream form
+    app.get('/dreams/new', (req, res) => {
+        res.render('dream-new', {})
+    });
+
+    app.post('/dreams', (req, res) => {
+        Dream.create(req.body).then((dream) => {
+            console.log(Object.entries(req.body))
+            res.redirect('/dreams')
+        }).catch((err) => {
+            console.log(err.message)
+        })
+    });
+
+    app.get('/dreams/:id', (req, res) => {
+        console.log('This is the id ' + req.params.id)
+        Dream.findById({
+            _id: ObjectId(req.params.id)
+        }).then((dream) => {
+            console.log('This is the fetched dream ' + dream)
+            res.render('dream-show', {
+                dream: dream
+            })
+        }).catch((err) => {
+            console.log(err.message)
+        })
+        // res.send('Hello world')
+    });
+
+    app.put('/dreams/:id', (req, res) => {
+        Dream.findOneAndUpdate(req.params.id, req.body)
+            .then(dream => {
+                console.log('DREAM =>>>> ' + dream)
+                res.redirect(`/dreams/${dream._id}`)
+            })
+            .catch(err => {
+                console.log(err.message)
+            })
+    })
+
+
+    app.get('/dreams/:id/edit', (req, res) => {
+        console.log('This is the id ' + req.params.id)
+        Dream.findById({
+            _id: ObjectId(req.params.id)
+        }).then((dream) => {
+            console.log('This is the edited dream ' + dream)
+            res.render('dreams-edit', {
+                dream: dream
+            })
+        }).catch((err) => {
+            console.log(err.message)
+        })
+        // res.send('Hello world')
+    });
+
+}
