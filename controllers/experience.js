4e7e862caa0ceb63e88ba1f2d5556b5041bd0e25
Matthew Harrilal <matthewharrilal@gmail.com>
2018-10-14 21:19:59 -0700

    Issue being able to render specific experience able to see all dreams when posting a new experience

--- controllers/experience.js
+++ controllers/experience.js
@@ -5,14 +5,38 @@ var ObjectId = require('mongodb').ObjectId
 module.exports = function(app) {
 
     app.get('/dreams/:id/experiences', (req, res) => {
-        Experience.find({dreamId: req.params.id}).then(experiences => {
+        const dreamId = req.params.id
+        Experience.find({
+            dreamId: req.params.id
+        }).then(experiences => {
             console.log('These are all the experiences ' + experiences)
             res.render('experiences-index', {
-                experiences
+                experiences,
+                dreamId
             })
         }).catch(err => {
             console.log(err)
         })
+    });
+
+    app.get('/dreams/:dreamId/experiences/:id', (req, res) => {
+        const dreamId = req.params.dreamId
+        Experience.find({
+                dreamId
+            }).then((experiences) => {
+                experiences.find(ObjectId(req.params.id)).then((experience) => {
+                        res.render("experience-show", {
+                            experiences
+                        })
+                    })
+                    .catch((err) => {
+                        console.log(err.message)
+                    })
+            })
+            .catch((err) => {
+                console.log(err.message)
+            })
+
     });
 
     app.post("/dreams/:id/experience", (req, res) => {
@@ -20,7 +44,8 @@ module.exports = function(app) {
         console.log('Initiating posting an experience')
         Experience.create(req.body).then((experience) => {
             console.log(Object.entries(req.body))
-            res.redirect(`/dreams/${req.params.id}/experiences`)
+            // res.redirect(`/dreams/${req.params.id}/experiences`)
+            res.redirect(`/dreams`)
         }).catch((err) => {
             console.log(err.message)
         })
