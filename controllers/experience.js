22a8c0eb5c80f9b47272c04cff6480def85867ec
Matthew Harrilal <matthewharrilal@gmail.com>
2018-10-14 18:43:53 -0700

    Able to save experience to database

--- controllers/experience.js
+++ controllers/experience.js
@@ -2,14 +2,35 @@ const Experience = require('../models/experience')
 var ObjectId = require('mongodb').ObjectId
 
 module.exports = function(app) {
-    // app.get("/experiences", (req, res) => {
-    //     '''Retrieves a collection of experiences'''
-    // });
 
-    app.get("/dreams/:id/experience/new", (req,res) => {
+    app.get('/dreams/:id/experiences', (req, res) => {
+        Experience.find().then(experiences => {
+            // console.log('These are all the dreams ' + dreams)
+            // res.render('experiences-index', {
+            //     experiences
+            // })
+            res.send('Hello World')
+        }).catch(err => {
+            console.log(err)
+        })
+    });
+
+    app.post("/dreams/:id/experience", (req, res) => {
+        console.log('Initiating posting an experience')
+        Experience.create(req.body).then((experience) => {
+            console.log(Object.entries(req.body))
+            res.send('Succesful')
+        }).catch((err) => {
+            console.log(err.message)
+        })
+    });
+
+    app.get("/dreams/:id/experience/new", (req, res) => {
         // res.send('Hello World')
         const dreamId = req.params.id
-        res.render('experience-new', {dreamId})
+        res.render('experience-new', {
+            dreamId
+        })
     });
 
 
